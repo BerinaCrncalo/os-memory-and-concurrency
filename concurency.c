9496888d8ccc9ac9a3b651dd8174a03fa5c62ed1
BerinaCrncalo <berina.crncalo@stu.ibu.edu.ba>
2024-05-23 18:56:32 +0200

    UPDATED concurency code

--- concurency.c
+++ concurency.c
@@ -1,13 +1,10 @@
 #include <semaphore.h>
 #include <pthread.h>
 #include <stdio.h>
-#include <stdlib.h>  // Include this for rand
-#include <time.h>    // Include this for seeding the random number generator
+#include <stdlib.h>
+#include <time.h>
 
-sem_t tobacco, paper, matches;
-sem_t agentSem;
-sem_t tobaccoSem, paperSem, matchesSem;
-sem_t mutex;
+sem_t tobacco, paper, matches, agentSem, mutex;
 
 void* agent(void* arg) {
     while (1) {
@@ -26,37 +23,14 @@ void* agent(void* arg) {
     }
 }
 
-void* smoker_with_tobacco(void* arg) {
+void* smoker(void* arg) {
+    sem_t *first = ((sem_t**)arg)[0];
+    sem_t *second = ((sem_t**)arg)[1];
     while (1) {
-        sem_wait(&paper);
-        sem_wait(&matches);
+        sem_wait(first);
+        sem_wait(second);
         sem_wait(&mutex);
-        // make and smoke cigarette
-        printf("Smoker with tobacco smokes\n");
-        sem_post(&agentSem);
-        sem_post(&mutex);
-    }
-}
-
-void* smoker_with_paper(void* arg) {
-    while (1) {
-        sem_wait(&tobacco);
-        sem_wait(&matches);
-        sem_wait(&mutex);
-        // make and smoke cigarette
-        printf("Smoker with paper smokes\n");
-        sem_post(&agentSem);
-        sem_post(&mutex);
-    }
-}
-
-void* smoker_with_matches(void* arg) {
-    while (1) {
-        sem_wait(&tobacco);
-        sem_wait(&paper);
-        sem_wait(&mutex);
-        // make and smoke cigarette
-        printf("Smoker with matches smokes\n");
+        printf("Smoker smokes\n");
         sem_post(&agentSem);
         sem_post(&mutex);
     }
@@ -71,10 +45,14 @@ int main() {
     sem_init(&agentSem, 0, 1);
     sem_init(&mutex, 0, 1);
 
+    sem_t *args_tobacco[] = {&paper, &matches};
+    sem_t *args_paper[] = {&tobacco, &matches};
+    sem_t *args_matches[] = {&tobacco, &paper};
+
     pthread_create(&agent_thread, NULL, agent, NULL);
-    pthread_create(&smoker_tobacco, NULL, smoker_with_tobacco, NULL);
-    pthread_create(&smoker_paper, NULL, smoker_with_paper, NULL);
-    pthread_create(&smoker_matches, NULL, smoker_with_matches, NULL);
+    pthread_create(&smoker_tobacco, NULL, smoker, args_tobacco);
+    pthread_create(&smoker_paper, NULL, smoker, args_paper);
+    pthread_create(&smoker_matches, NULL, smoker, args_matches);
 
     pthread_join(agent_thread, NULL);
     pthread_join(smoker_tobacco, NULL);
